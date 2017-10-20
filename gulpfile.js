bdea7ff1401c1783aa731a652eaead72d16d03d3
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-10-20 09:59:50 +0100

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,12 @@ gulp.task("pre-install", shell.task([
 
 gulp.task("serve", shell.task("static-server"));
 
+gulp.task("the-protocol", shell.task("nodemon the-protocol/express-server.js"));
+gulp.task("get", shell.task("nodemon the-protocol/get.js"));
+gulp.task("post", shell.task("nodemon the-protocol/post.js"));
+
+gulp.task("browsers-and-http", shell.task("nodemon browsers-and-http/server.js"));
+gulp.task("xmlhttprequest", shell.task("nodemon xmlhttprequest/server.js"));
+gulp.task("sending-a-request", shell.task("nodemon sending-a-request/server.js"));
+
 gulp.task("lint", shell.task("jshint *.js **/*.js"));
